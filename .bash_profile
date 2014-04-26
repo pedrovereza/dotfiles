a25de56425a2a53091174a0fdf3c6768fd15f060
Pedro Vereza <pdrvereza@gmail.com>
2014-04-26 19:55:25 -0300

    Shamelessly stealing Erick's install script. Plus adding z.sh

--- .bash_profile
+++
@@ -1,7 +0,0 @@
-. ~/dotfiles/alias/all
-. ~/dotfiles/completion/all
-. ~/dotfiles/colors/all
-
-[[ -s "$HOME/.rvm/scripts/rvm" ]] && source "$HOME/.rvm/scripts/rvm" # Load RVM into a shell session *as a function*
-export PATH=/Applications/Racket\ v6.0/bin:$PATH
-export PATH=/usr/local/bin:$PATH
--- install.sh
+++ install.sh
@@ -1,34 +1,15 @@
-#!/usr/bin/env bash
-
 DOTFILES_HOME=$(cd `dirname ${BASH_SOURCE[0]}`; pwd)
+
 BASH_PROFILE="$HOME/.bash_profile"
+DOTFILES_INIT="$DOTFILES_HOME/init"
 
-_create_all_file_for () {
-	CURRENT_DIR=$(cd  ${DOTFILES_HOME}; pwd)
-	CURRENT_DIR+=/$1
+if [ ! -f $BASH_PROFILE ]; then
+	echo 'Creating .bash_profile file'
+	touch $BASH_PROFILE
+fi
 
-	all_file="$CURRENT_DIR/all"
-
-	if [ ! -f "$all_file" ]; then
-		echo "generating all file for $1"
-
-		for file in `find "$CURRENT_DIR" -type f`; do
-			echo "adding $file to all file"
-			
-			echo ". $file" >> "$all_file"
-		done
-	fi
-}
-
-_copy_bash_profile() {
-	cp -f $DOTFILES_HOME/.bash_profile $BASH_PROFILE
-}
-
-_copy_bash_profile
-
-for dir in 'alias' 'completion' 'colors'; do
-	_create_all_file_for $dir
-done
+if ! grep -Fq $DOTFILES_INIT $BASH_PROFILE; then
+	echo "source $DOTFILES_INIT" >> $BASH_PROFILE
+fi
 
 source $BASH_PROFILE
-
